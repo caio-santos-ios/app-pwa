1d838eb57fe44c5f3fe39aa3015008dc2ae8ebd9
caio-santos-ios <caiosantos.dev@outlook.com>
2026-01-19 18:45:26 -0300

    feat: initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,14 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  disable: process.env.NODE_ENV === "development", 
+  disable: process.env.NODE_ENV === "development",
 });
 
 export default withPWA({
-  // Suas outras configurações do Next.js aqui
+  // Se o erro de tipo persistir no 'turbo', 
+  // use o comentário abaixo para o TypeScript ignorar a linha
+  experimental: {
+    // @ts-ignore
+    turbo: {}, 
+  },
 });
