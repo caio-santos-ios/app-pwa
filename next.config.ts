6fb3d21ddf536f3bb05881008ec365f8c12d628f
caio-santos-ios <caiosantos.dev@outlook.com>
2026-01-19 18:40:49 -0300

    feat: initial commit

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development", 
+});
+
+export default withPWA({
+  // Suas outras configurações do Next.js aqui
+});
