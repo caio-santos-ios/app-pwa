dd2c52e035013d6278f5a1308238c65c857c5640
caio-santos-ios <caiosantos.dev@outlook.com>
2026-01-19 19:30:17 -0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,15 @@ const withPWA = withPWAInit({
 });
 
 export default withPWA({
-  // Se o erro de tipo persistir no 'turbo', 
-  // use o comentário abaixo para o TypeScript ignorar a linha
+  // Crucial para rodar em pasbem.com.br/aplicativo
+  basePath: '/aplicativo',
+  
   experimental: {
     // @ts-ignore
     turbo: {}, 
+  },
+  // Garante que o build use Webpack para processar o Service Worker do PWA
+  webpack: (config) => {
+    return config;
   },
 });
