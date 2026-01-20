2ba23e91484f896905e0c4b561aaa3332c3ca63c
caio-santos-ios <caiosantos.dev@outlook.com>
2026-01-20 08:02:25 -0300

    feat: add tela de login

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import withPWAInit from "@ducanh2912/next-pwa";
 const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
+  // Adicione estas linhas para mapear os caminhos corretamente
+  sw: "sw.js", 
+  register: true,
+  scope: "/aplicativo/",
 });
 
 export default withPWA({
