d90b9bc83e430541c44ba6d6f71017cdd7a5fc07
caio-santos-ios <caiosantos.dev@outlook.com>
2026-01-20 08:11:40 -0300

    feat: add tela de login

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,19 @@ import withPWAInit from "@ducanh2912/next-pwa";
 const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
-  // Adicione estas linhas para mapear os caminhos corretamente
-  sw: "sw.js", 
-  register: true,
-  scope: "/aplicativo/",
 });
 
 export default withPWA({
+  // Se o erro de tipo persistir no 'turbo', 
+  // use o comentário abaixo para o TypeScript ignorar a linha
+  // Crucial para rodar em pasbem.com.br/aplicativo
   basePath: '/aplicativo',
   
   experimental: {
     // @ts-ignore
     turbo: {}, 
   },
-  
+  // Garante que o build use Webpack para processar o Service Worker do PWA
   webpack: (config) => {
     return config;
   },
